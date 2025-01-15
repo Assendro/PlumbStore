0603ad8abb00c545958e65f00db468827ebb532c
Assendro <kirilll999127@gmail.com>
2025-01-15 19:34:38 +0300

    Тестирую коммит

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import React from 'react';
 
 const App = () => {
-    return <h1>test</h1>;
+    return <h1>te123st</h1>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
